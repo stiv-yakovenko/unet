029e56987a9ac602c304fdffea7231c52ecac7b8
Stepan Yakovenko <stiv.yakovenko@gmail.com>
2019-05-09 03:22:09 +0700

    intermed

--- main.py
+++ main.py
@@ -11,12 +11,12 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/membrane/train','image','label',data_gen_args,save_to_dir = None)
+myGene = trainGenerator(2,'data/cracks/train','image','label',data_gen_args,save_to_dir = None)
 
 model = unet()
-model_checkpoint = ModelCheckpoint('unet_membrane.hdf5', monitor='loss',verbose=1, save_best_only=True)
+model_checkpoint = ModelCheckpoint('unet_cracks.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=300,epochs=1,callbacks=[model_checkpoint])
 
-testGene = testGenerator("data/membrane/test")
+testGene = testGenerator("data/cracks/test")
 results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/membrane/test",results)
+saveResult("data/cracks/test",results)
