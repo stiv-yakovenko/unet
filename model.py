00ba7ebb84cb033f04e00b6fe144474546c45e62
Stepan Yakovenko <stiv.yakovenko@gmail.com>
2019-05-13 06:46:04 +0700

    some more data

--- model.py
+++ model.py
@@ -86,7 +86,7 @@ def unet(pretrained_weights = None,input_size = (256,256,3)):
 
     model = Model(input = inputs, output = conv10)
 
-    model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
+    model.compile(optimizer = Adam(lr = 3e-4), loss = 'binary_crossentropy', metrics = ['accuracy'])
     
     #model.summary()
 
