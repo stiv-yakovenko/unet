d492761f82b15686b19280f4a931cd1cd4980cf8
Stepan Yakovenko <stiv.yakovenko@gmail.com>
2019-05-12 19:13:13 +0700

    more data

--- main.py
+++ main.py
@@ -1,9 +1,13 @@
+import sys
+
 from model import *
 from data import *
 
 #os.environ["CUDA_VISIBLE_DEVICES"] = "0"
-
-
+if (len(sys.argv)<2):
+    print("set dataset folder")
+    sys.exit(0)
+dataset = sys.argv[0]
 data_gen_args = dict(rotation_range=0.2,
                     width_shift_range=0.05,
                     height_shift_range=0.05,
@@ -11,12 +15,12 @@ data_gen_args = dict(rotation_range=0.2,
                     zoom_range=0.05,
                     horizontal_flip=True,
                     fill_mode='nearest')
-myGene = trainGenerator(2,'data/cracks/train','image','label',data_gen_args,save_to_dir = None,image_color_mode='rgb')
-testGene = testGenerator("data/cracks/test",as_gray=False)
+myGene = trainGenerator(2,'data/'+dataset+'/train','image','label',data_gen_args,save_to_dir = None,image_color_mode='rgb')
+testGene = testGenerator("data/"+dataset+"/test",as_gray=False)
 
 model = unet()
 model_checkpoint = ModelCheckpoint('unet_cracks.hdf5', monitor='loss',verbose=1, save_best_only=True)
 model.fit_generator(myGene,steps_per_epoch=900,epochs=12,callbacks=[model_checkpoint])
 
 results = model.predict_generator(testGene,30,verbose=1)
-saveResult("data/cracks/test",results)
+saveResult("data/"+dataset+"/test",results)
