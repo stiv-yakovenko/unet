9dd0331b55a76fd74859af23ddbb0f0bd169b314
Stepan Yakovenko <stiv.yakovenko@gmail.com>
2019-06-13 01:50:10 +0300

    intermed commit

--- load_model.py
+++ load_model.py
@@ -32,19 +32,24 @@ def inference_by_blocks(png_file,pb_folder, out_png_file):
             result = Image.fromarray((p1).astype(np.uint8))
             new_im.paste(result, tile.coords)
     new_im.save(out_png_file)
-
+os.environ["OMP_NUM_THREADS"]="1"
+config = tf.ConfigProto()
+config.intra_op_parallelism_threads = 1
+config.inter_op_parallelism_threads = 1
 def inference_single():
     loaded_graph = tf.Graph()
-    with tf.Session(graph=loaded_graph) as sess:
+    with tf.Session(graph=loaded_graph,config=config) as sess:
         tf.saved_model.loader.load(sess, [tf.saved_model.tag_constants.TRAINING], "./models/roofs_not_roofs/")
         x = loaded_graph.get_tensor_by_name('x:0')
         y = loaded_graph.get_tensor_by_name('y:0')
         predictor = loaded_graph.get_tensor_by_name('results/pixel_wise_softmax/predicter:0')
         keep_prob = loaded_graph.get_tensor_by_name('dropout_probability:0')
-        img=Image.open("./test_pic/256x256.png").convert("RGB")
+        img=Image.open("./test_pic/b2_DJI_0145_02_05.png").convert("RGB")
         f = np.array(img) / 256.
         inp = f[None, ...]
         y_dummy = np.empty((inp.shape[0], inp.shape[1], inp.shape[2], 2))
         res = sess.run(predictor, feed_dict={x: inp, keep_prob: 1.0, y: y_dummy})[0, ..., 1]
-        print(res.shape)
+        result = Image.fromarray((res*256.).astype(np.uint8))
+        result.save("res.png")
+        print("saved")
 inference_single()
